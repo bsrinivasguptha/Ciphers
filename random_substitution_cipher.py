7096d2bc2b70cf205cbfefd7a8f362a8d6eac61a
tbhall <tbhall@email.sc.edu>
2017-05-27 19:28:43 -0400

    Fixed other character issue

--- random_substitution_cipher.py
+++ random_substitution_cipher.py
@@ -26,33 +26,6 @@ import copy
 ###################
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-freq_letter = {'e' : 12.702,
-                't' : 9.056,
-                'a' : 8.167,
-                'o' : 7.507,
-                'i' : 6.966,
-                'n' : 6.749,
-                's' : 6.327,
-                'h' : 6.094,
-                'r' : 5.987,
-                'd' : 4.253,
-                'l' : 4.025,
-                'c' : 2.782,
-                'u' : 2.758,
-                'm' : 2.406,
-                'w' : 2.360,
-                'f' : 2.228,
-                'g' : 2.015,
-                'y' : 1.974,
-                'p' : 1.929,
-                'b' : 1.492,
-                'v' : 0.978,
-                'k' : 0.772,
-                'j' : 0.153,
-                'x' : 0.150,
-                'q' : 0.095,
-                'z' : 0.074
-                }
 key_dic = {'a' : '0',
                 'b' : '1',
                 'c' : '2',
@@ -79,6 +52,34 @@ key_dic = {'a' : '0',
                 'x' : '23',
                 'y' : '24',
                 'z' : '25'
+                }
+### TO BE USED LATER ###
+freq_letter = {'e' : 12.702,
+                't' : 9.056,
+                'a' : 8.167,
+                'o' : 7.507,
+                'i' : 6.966,
+                'n' : 6.749,
+                's' : 6.327,
+                'h' : 6.094,
+                'r' : 5.987,
+                'd' : 4.253,
+                'l' : 4.025,
+                'c' : 2.782,
+                'u' : 2.758,
+                'm' : 2.406,
+                'w' : 2.360,
+                'f' : 2.228,
+                'g' : 2.015,
+                'y' : 1.974,
+                'p' : 1.929,
+                'b' : 1.492,
+                'v' : 0.978,
+                'k' : 0.772,
+                'j' : 0.153,
+                'x' : 0.150,
+                'q' : 0.095,
+                'z' : 0.074
                 }
 
 ###################
@@ -117,10 +118,11 @@ def encrypt(raw, key):
 
     # Creates the encrypted string
     for raw_char in raw:
-        if raw_char == ' ' or raw_char == '.':
-            encrypt_string += raw_char
-        else:
+        if raw_char.isalpha():
             encrypt_string += local_dic[raw_char.lower()]
+        else:
+            encrypt_string += raw_char
+
 
     # Calls method to write encrypted string with the key in format:
     # encrypt_string:key
@@ -159,13 +161,14 @@ def decrypt(encrypted, key):
 
     # Creates the decrypted string
     for encrypt_char in encrypted:
-        if encrypt_char == ' ' or encrypt_char == '.':
-            decrypt_string += encrypt_char
-        else:
+        if encrypt_char.isalpha():
             for dic_key, dic_value in local_dic.items():
                 if dic_value == encrypt_char:
                     decrypt_string += dic_key
                     break
+        else:
+            decrypt_string += encrypt_char
+
     # Returns the decrypted string
     return decrypt_string
 
